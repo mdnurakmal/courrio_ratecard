be4ccfa91cf653719cef7c7a2186099ac1cc2f2b
Nur Akmal <2183322C@student.tp.edu.sg>
2022-01-27 20:35:02 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const https = require('https');
 var moment = require('moment-timezone');
+moment.weekdays(true);
 const http = require('http');
 const express = require("express");
 const bodyParser = require("body-parser");
