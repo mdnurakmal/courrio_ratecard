d47e8f6ec7e5ece48b22e1f9111dcba4785302d7
Nur Akmal <2183322C@student.tp.edu.sg>
2022-01-27 19:55:18 +0800

    Update app.js

--- app.js
+++ app.js
@@ -32,34 +32,42 @@ async function calculateDistance(ori,des) {
     var totalDistance = 0;
     var origins = [ori];
     var destinations =[des];
-    await distance.matrix(origins,destinations, await function(err, distances) {
+    return new Promise((resolve, reject) => {
+        distance.matrix(origins,destinations,  function(err, distances) {
 
-        if (err) {
-            return console.log(err);
-        }
-        if (!distances) {
-            return console.log('no distances');
-        }
+            if (err) {
 
-        if (distances.status == 'OK') {
-            for (var i=0; i < origins.length; i++) {
-                for (var j = 0; j < destinations.length; j++) {
-                    var origin = distances.origin_addresses[i];
-                    var destination = distances.destination_addresses[j];
-                    if (distances.rows[0].elements[j].status == 'OK') {
-                        var distance = distances.rows[i].elements[j].distance.text;
-                        console.log('Distance from ' + origin + ' to ' + destination + ' is ' + distance);
-                    } else {
-                        console.log(destination + ' is not reachable by land from ' + origin);
+                reject(err);
+            }
+            if (!distances) {
+                reject('no distances');
+            }
+    
+            if (distances.status == 'OK') {
+                for (var i=0; i < origins.length; i++) {
+                    for (var j = 0; j < destinations.length; j++) {
+                        var origin = distances.origin_addresses[i];
+                        var destination = distances.destination_addresses[j];
+                        if (distances.rows[0].elements[j].status == 'OK') {
+                            var distance = distances.rows[i].elements[j].distance.text;
+                            console.log('Distance from ' + origin + ' to ' + destination + ' is ' + distance);
+                        } else {
+                            console.log(destination + ' is not reachable by land from ' + origin);
+                        }
                     }
                 }
             }
-        }
-
-        totalDistance= distances.rows[0].elements[0].distance.text.split(" ")[0];
-        console.log(totalDistance);
-        return totalDistance;
+    
+            totalDistance= distances.rows[0].elements[0].distance.text.split(" ")[0];
+            console.log(totalDistance);
+            resolve(totalDistance);
+        });
+    })
+    .catch(function(err) {
+        console.log(err);
+        return;
     });
+       
 
 
     
