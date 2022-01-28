e17e974972c6932d63f4e12c36f0e93e3cc6120d
Nur Akmal <2183322C@student.tp.edu.sg>
2022-01-28 16:42:19 +0800

    Update app.js

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ function computeDeliveryDate(rate,fixedDeadline,orderCutOff,deliveryDeadline,ord
 {
     // same day delivery and delivery dateline set to 1700
     console.log(rate + " , " + fixedDeadline  + " , " + orderDate.format('MMMM Do YYYY, h:mm:ss a') + ", " +orderCutOff)
-    var temp = orderDate.set({"day":31 , "hour": 22, "minute": 0,"second":0})
-    console.log(temp.format('MMMM Do YYYY, h:mm:ss a') + ">>");
+    // var temp = orderDate.set({"day":31 , "hour": 22, "minute": 0,"second":0})
+    // console.log(temp.format('MMMM Do YYYY, h:mm:ss a') + ">>");
     var deliveryDate;
 
     var cutoff;
@@ -58,7 +58,7 @@ function computeDeliveryDate(rate,fixedDeadline,orderCutOff,deliveryDeadline,ord
         deliveryDate.set({"hour": 17, "minute": 0,"second":0});
     }
 
-    var isBefore = moment(temp).isBefore(cutoff);
+    var isBefore = moment(orderDate).isBefore(cutoff);
 
     if(isBefore)
     {
